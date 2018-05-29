cd7b222977b963de7eb2172ebddd342ba1e166c7
magentall <35685070+magentall@users.noreply.github.com>
2018-05-29 12:12:20 +0200

    Update req.php

--- api/inc/req.php
+++ api/inc/req.php
@@ -1,7 +1,7 @@
 <?php
 
 function req($query){
-  $conn = new mysqli("localhost", "root", "BoomBoom11**", "ctdje");
+  $conn = new mysqli("localhost", "root", "", "ctdje");
   return $conn->query($query);
 }
 
